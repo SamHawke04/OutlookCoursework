7f780c7f814cc0ff566948258316bee019754197
Samuel E Hawke <sh4175w@gre.ac.uk>
2025-11-10 15:17:29 +0000

    Added all Variables.
    Mail.getAll() returns all of the variables in a neat array
    
    Tested in Test.py
    
    Signed-off-by: Samuel E Hawke <sh4175w@gre.ac.uk>

--- Mail.py
+++ Mail.py
@@ -1,15 +1,24 @@
 class Mail:
-    def __init__(self, text, subject, from_email, to_email):
-        self.text = text
-        self.subject = subject
+    def __init__(self, id, from_email, to_email, date, subject, tag, body, flag, read):
+        self.id = id
         self.from_email = from_email
         self.to_email = to_email
+        self.date = date
+        self.subject = subject
+        self.tag = tag
+        self.body = body
+        self.flag = flag
+        self.read = read
+
+    def getAll(self):
+        return self.id, self.from_email, self.to_email, self.date, self.subject, self.tag, self.body, self.flag, self.read
 
 class Confidential(Mail):
-    def __init__(self, text, subject, from_email, to_email):
-        super().__init__(text, subject, from_email, to_email)
+    def __init__(self, from_email, to_email, date, subject, tag, body):
+        super().__init__(from_email, to_email, date, subject, tag, body)
 
 class Personal(Mail):
-    def __init__(self, text, subject, from_email, to_email):
-        super().__init__(text, subject, from_email, to_email)
+    def __init__(self, from_email, to_email, date, subject, tag, body):
+        super().__init__(from_email, to_email, date, subject, tag, body)
+
 
